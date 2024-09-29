4fa51995eef38fb88176f9b4d03df8a9529af3bb
yor5e <ttty28906@gmail.com>
2024-09-29 02:14:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 
 let body = document.querySelector('body');
-let fingerprint = document.querySelector('.fingerprint');
-let center = document.querySelector('.center');
-let scan = document.querySelector('.scan');
-let timer, timerSuccesss;
-
-
-function onSuccess() {
-    body.removeEventListener('mousedown', onTouchstart);
-    body.removeEventListener('touchstart', onTouchstart);
-
-
-    fingerprint.classList.remove('active');
-    center.classList.add('success')
-
-    clearTimeout(timerSuccesss);
-
-    timerSuccesss = setTimeout(() => {
-        body.addEventListener('mousedown', onTouchstart);
-        body.addEventListener('touchstart', onTouchstart);
-        center.classList.remove('success')
-
-    },3000);
-}
-
+le
 function onTouchstart () {
     fingerprint.classList.add('active');
     timer = setTimeout(onSuccess,2000)
