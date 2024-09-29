c96c6e975d85e61b41ffcf26b157e2ee90b8e72f
yor5e <ttty28906@gmail.com>
2024-09-29 02:14:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,3 @@ function onTouchEnd() {
     fingerprint.classList.remove('active')
     clearTimeout(timer)
 }
-
-body.addEventListener('mousedown', onTouchstart)
-body.addEventListener('touchstart', onTouchstart)
-body.addEventListener('mouseup', onTouchEnd)
-body.addEventListener('touchend', onTouchEnd)
